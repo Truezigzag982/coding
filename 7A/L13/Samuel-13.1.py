733fde1e03097e6052d4536f3dd6947dd70820ba
Samuel <samuel@muzhuen.com>
2022-04-08 10:03:14 +0700

    Update Samuel-13.1.py

--- 7A/L13/Samuel-13.1.py
+++ 7A/L13/Samuel-13.1.py
@@ -1,18 +1,22 @@
-a = ()
-
-def factorial(n, a):
-    if a < n:
-        c + 1
-        a.append(c)
+def factorial(n):
         if n in (0, 1):
             return 1
         else:
             return n*factorial(n-1)
-    else:
-        return a
+
+def factorial_sum(n):
+    result = 0
+    expression = ""
+    for i in range (n+1):
+        result += factorial(i)
+        if i > 0:
+            expression += '+'
+        expression += f'{i}!'
+    print (f'{expression}={result}')
 
 
 
 
 n = int(input('A number: '))
-print(f'{n}! ' + '! + '.join(a) + f'={factorial(n, a)}')
+factorial_sum(n)
+#print(f'{factorial_sum(n)}={factorial(n)}')
